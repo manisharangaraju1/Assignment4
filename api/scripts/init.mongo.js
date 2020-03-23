85b5b50c6aecab2963efe1ec81399647f004d81d
Manisha <manisha.rangaraju@gmail.com>
2020-03-23 00:01:07 -0700

    final version

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -1,25 +1,24 @@
-print(db)
-// db.products.remove({});
+db.products.remove({});
 
-// // const productsDB = [
-// //     {
-// //       id: 1, 
-// //       Name: "Clothes",
-// //       Price: 20,
-// //       Category: "Shirts",
-// //       Image: "",
-// //     },
-// //   ];
+const productsDB = [
+    {
+      id: 1, 
+      Name: "Clothes",
+      Price: 20,
+      Category: "Shirts",
+      Image: "",
+    },
+  ];
 
-// // db.products.insertMany(productsDB);
-// const count = db.products.count();
-// // print('Inserted', count, 'products');
+db.products.insertMany(productsDB);
+const count = db.products.count();
+print('Inserted', count, 'products');
 
-// db.counters.remove({ _id: 'products' });
-// db.counters.insert({ _id: 'products', current: count });
+db.counters.remove({ _id: 'products' });
+db.counters.insert({ _id: 'products', current: count });
 
-// db.products.createIndex({ id: 1 }, { unique: true });
-// db.products.createIndex({ Name: 1 });
-// db.products.createIndex({ Price: 1 });
-// db.products.createIndex({ Category: 1 });
-// db.products.createIndex({ Image: 1 });
+db.products.createIndex({ id: 1 }, { unique: true });
+db.products.createIndex({ Name: 1 });
+db.products.createIndex({ Price: 1 });
+db.products.createIndex({ Category: 1 });
+db.products.createIndex({ Image: 1 });
